eea1e3dd45dee711ca4cb1c49d45f30fe56bc630
KulikSS0395 <ds_347@mail.ru>
2021-09-13 15:11:09 +0300

    1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,19 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 = (HelloWorld)  applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+
+        Cat beanCAt2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean == bean2);
+        System.out.println(bean.equals(bean2));
+
+        System.out.println(beanCat == beanCAt2);
+        System.out.println(beanCat.equals(beanCAt2));
+
     }
 }
